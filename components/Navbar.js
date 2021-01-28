5aacb107142fe6d1db234cda80e5f5ae82883f70
Kenny Whyte <kawhyte@gmail.com>
2021-01-28 12:15:41 -0800

    Testing Components

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<nav>
+			<div className='logo'>
+				<h1>Mario List</h1>
+			</div>
+			<a>Home</a>
+			<a>About</a>
+			<a>Mario Listings</a>
+		</nav>
+	);
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div >
-<h1> Home Page</h1>
-<p> The time you've been taking to ship some of your items is different than your listed processing times. Accurate processing times can drive sales and prevent cases by helping meet buyer expectations.</p>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<h1> Home Page</h1>
+			<p>
+				The time you've been taking to ship some of your items is different than
+				your listed processing times. Accurate processing times can drive sales
+				and prevent cases by helping meet buyer expectations.
+      </p> 
+      <Footer />
+		</div>
+	);
 }
