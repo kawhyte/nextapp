4bef45c6fe32e27c9e59ef00f1d03bc71ffa51ee
Kenny Whyte <kawhyte@gmail.com>
2021-01-28 12:08:08 -0800

    Testing Routes in Next

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1> About Page</h1>
+			<p>
+				The time you've been taking to ship some of your items is different than
+				your listed processing times. Accurate processing times can drive sales
+				and prevent cases by helping meet buyer expectations.
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ pages/kenny/text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function text() {
+    return (
+        <div>
+            <h1>Kenny test</h1>
+        </div>
+    )
+}
+
+export default text
