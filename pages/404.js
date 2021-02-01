abb4419ed23dd1aa5972408b5b5fd2b716bcb847
Kenny Whyte <kawhyte@gmail.com>
2021-02-01 12:08:25 -0800

    redirecting to home page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,21 @@
 import Link from "next/Link";
 
+import {useEffect} from 'react';
+import {useRouter} from 'next/router'
+
 
 const NotFound = () => {
+
+    const router = useRouter()
+
+useEffect(() => {
+    setTimeout(() => {
+        router.push('/')
+    }, 3000);
+  
+}, [])
+
+
     return ( <div className ="not-found">
         
     <h1> Not found</h1>
