0cb87f531935f03b294ade0dc4a132535c9bb31d
Kenny Whyte <kawhyte@gmail.com>
2021-02-04 12:18:09 -0800

    Dynamic Routes -2

--- pages/zelda/[id].js
+++ pages/zelda/[id].js
@@ -1,9 +1,25 @@
+export const getStaticPath = async () => {
+	const response = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await response.json();
+
+	const paths = data.map((zelda) => {
+		return {
+			params: { id: zelda.id.toString() },
+		};
+	});
+
+	return {
+		paths: paths,
+		fallback: false,
+	};
+};
+
 const Details = () => {
-    return ( 
-        <div>
-        <h1>Details Page</h1>
-        
-        </div> );
-}
- 
+	return (
+		<div>
+			<h1>Details Page</h1>
+		</div>
+	);
+};
+
 export default Details;
