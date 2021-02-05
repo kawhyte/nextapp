9e76f564fe4a1068e134e35692137be5ab77670f
Kenny Whyte <kawhyte@gmail.com>
2021-02-05 12:33:42 -0800

    user agent

--- pages/zelda/[id].js
+++ pages/zelda/[id].js
@@ -1,5 +1,21 @@
 export const getStaticPaths = async () => {
-	const response = await fetch("https://zelda-api.apius.cc/api/games");
+	const response = await fetch("https://zelda-api.apius.cc/api/games",
+	
+	{
+        method: "GET",
+        headers: {
+          // update with your user-agent
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+          Accept: "application/json; charset=UTF-8",
+        },
+      }
+	
+	
+	
+	
+	
+	);
 	const data = await response.json();
 
 	const paths = data.data.map((zelda) => {
@@ -19,7 +35,21 @@ export const getStaticProps = async (context) => {
 	const id = context.params.id;
 	
 	const response = await fetch(
-		"https://zelda-api.apius.cc/api/games/" + id
+		"https://zelda-api.apius.cc/api/games/" + id,
+
+		{
+			method: "GET",
+			headers: {
+			  // update with your user-agent
+			  "User-Agent":
+				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+			  Accept: "application/json; charset=UTF-8",
+			},
+		  }
+
+
+
+
 	);
 	const data = await response.json();
 
